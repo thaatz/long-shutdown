7851aff6f832c35b0c20be6fc35a6a25f710caf7
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-02-01 16:46:05 -0500

    commented potential methods of running chkdsk in poweroff.bat

--- poweroff.bat
+++ poweroff.bat
@@ -39,6 +39,9 @@ if not %ERRORLEVEL%==0 (
 	echo SFC: SFC completed sucessfully. >>"%longshutdownlog%"
 )
 
+:: make the log when you log in
+:: we use HKCU instead of HKLM so that its just for the user who ran this
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce /v "long-shutdown" /t "REG_SZ" /d "powershell.exe \"%~dp0\chklog.ps1\""
 chkdsk %SystemDrive%
 if /i not %ERRORLEVEL%==0 (
 	echo DEBUG: %errorlevel% %bugger%
@@ -46,6 +49,7 @@ if /i not %ERRORLEVEL%==0 (
 	fsutil dirty set %SystemDrive%
 	REM copy temp.bat "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
 	schtasks /create /tn "long-shutdown" /ru SYSTEM /sc ONSTART /tr "%cd%\task.bat" /RL HIGHEST
+	REM reg add HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce /v "long-shutdown" /t "REG_SZ" /d "%SystemRoot%\System32\shutdown /s /f"
 	%SystemRoot%\System32\shutdown /r /f
 ) else (
 	echo DEBUG: %errorlevel% %bugger%
